e55bfb24b4a70945f7abca267aa649f38dfa32e6
exelCode226 <exelagualimpia@gmail.com>
2024-09-16 19:48:24 -0500

    Se añadio el modulo de autenticacion

---
+++ src/middlewares/validateToken.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+import config from '../config/config.js';
+
+export const authMiddleware = (req, res, next) => {
+    const token = req.cookies.token || req.headers['authorization'];
+
+    if (!token) {
+        return res.status(401).json({ message: "Acceso denegado. No hay token." });
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.jwtSecret);
+        req.userId = decoded.id;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Token inválido." });
+    }
+};
